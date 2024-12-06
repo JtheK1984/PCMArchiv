b39eaf327e7ad4c44479f3f2fba0911878d90ca4
Jens Henske <Jens.Henske@outlook.com>
2024-12-06 08:27:09 +0100

    'autopush'

--- MSBuild.bat
+++ MSBuild.bat
@@ -1,15 +1,17 @@
 call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
 echo "Build erstellen"
-msbuild E:/Projekte/Windows/PCMArchiv/PCMArchiv.dproj /t:Clean;Build;Localize;CompressWin64 /p:config=Release /p:platform=Win64
 msbuild E:/Projekte/Windows/PCMArchiv/PCMArchiv.dproj /t:Clean;Build;CompressWin32 /p:config=Release /p:platform=Win32
+msbuild E:/Projekte/Windows/PCMArchiv/PCMArchiv.dproj /t:Clean;Build;CompressWin64;Localize /p:config=Release /p:platform=Win64
 
-echo "Kopiere Datei ins Setupverzeichnis"
-copy /y /v E:\Projekte\Windows\PCMArchiv\Win64\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
 copy /y /v E:\Projekte\Windows\PCMArchiv\Win32\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv"
 
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
+copy /y /v E:\Projekte\Windows\PCMArchiv\Win64\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 copy /y /v E:\Projekte\Windows\PCMArchiv\Win64\Release\PCMArchiv.DE "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 copy /y /v E:\Projekte\Windows\PCMArchiv\Win64\Release\PCMArchiv.EN "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 
+echo "Kopiere Doku ins Setupverzeichnis"
 copy /y /v E:\Projekte\Windows\PCMArchiv\PCMArchiv.docx "e:\Inno\Setupfiles\Programme\PCMArchiv"
 copy /y /v E:\Projekte\Windows\PCMArchiv\PCMArchiv.pdf "e:\Inno\Setupfiles\Programme\PCMArchiv"
 copy /y /v E:\Projekte\Windows\PCMArchiv\PCMArchiv.htm "e:\Inno\Setupfiles\Programme\PCMArchiv"

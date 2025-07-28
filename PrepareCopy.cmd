cd3b4ee9154d88a91cba891b7e817a1a96db6323
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-07-28 12:22:27 +0200

    Version 1.0

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,12 +1,13 @@
-echo "Kopiere Datei ins Setupverzeichnis"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
 copy /y /v Win32\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv"
-copy /y /v Win64\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
-
 copy /y /v Win32\Release\PCMArchiv.DE "e:\Inno\Setupfiles\Programme\PCMArchiv"
 copy /y /v Win32\Release\PCMArchiv.EN "e:\Inno\Setupfiles\Programme\PCMArchiv"
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
+copy /y /v Win64\Release\PCMArchiv.exe "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 copy /y /v Win64\Release\PCMArchiv.DE "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 copy /y /v Win64\Release\PCMArchiv.EN "e:\Inno\Setupfiles\Programme\PCMArchiv_x64"
 
+echo "Kopiere Doku ins Setupverzeichnis"
 copy /y /v PCMArchiv.docx "e:\Inno\Setupfiles\Programme\PCMArchiv"
 copy /y /v PCMArchiv.pdf "e:\Inno\Setupfiles\Programme\PCMArchiv"
 copy /y /v PCMArchiv.htm "e:\Inno\Setupfiles\Programme\PCMArchiv"
